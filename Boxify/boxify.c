d5a1f51d517f24ad0f4dffe5e395c99965aec554
Nuc1earOwl <nuclearowl1@gmail.com>
2024-09-29 15:59:12 +0200

    made code slightly faster
    literally just made every variable be stored in cpu registers

--- Boxify/boxify.c
+++ Boxify/boxify.c
@@ -2,16 +2,16 @@
 
 int main()
 {
-    int dimension;
+    register int dimension;
 
     printf("Welcome to the Boxify-er 3000, programmed by a total amateur in C!\n");
     printf("Enter a number to boxify:\n");
     scanf("%d", &dimension);
 
-    for (int y = 0; y < dimension; y++)
+    for (register int y = 0; y < dimension; y++)
     {
         printf("\n");
-        for (int x = 0; x < dimension; x++)
+        for (register int x = 0; x < dimension; x++)
         {
             if (y == 0)
             {
@@ -38,7 +38,7 @@ int main()
             }
         }
     }
-    printf("\n\n");
+    printf("\n\nThanks for using this program! Bub-bye :)");
     
     return 0;
 }
